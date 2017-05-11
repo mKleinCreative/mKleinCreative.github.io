7770f0778f3382d7b98cd9724625d8d0d78f5b21
Michael Klein <miketheklein@gmail.com>
2017-05-11 10:11:58 -0700

    Life is pain and darkness

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-document.addEventListener('mousemove', function (e) {
-    let w = getComputedStyle(document.documentElement).width.slice(0, -2),
-        h = getComputedStyle(document.documentElement).height.slice(0, -2),
-        rx = (h / 2 - e.pageY) / 50,
-        ry = (w / 2 - e.pageX) / 100;
-
-    main.style.transform = 'rotateX(' + rx + 'deg) rotateY(' + -ry + 'deg)';
-    mainImg.style.transform = 'translate3d(' + ry * 3 + 'px, ' + rx * 3 + 'px, 0)';
-});
+function moveWithMouse(key){
+  console.log( '---===key===---', key )
+  var theThing = document.querySelector(`[data-key=${key}]`)
+  console.log( '<3333333  <3333333', theThing )
+    document.addEventListener('mousemove', function (e) {
+          w = getComputedStyle(theThing).width.slice(0, -2),
+          h = getComputedStyle(theThing).height.slice(0, -2),
+          rx = (h / 2 - e.pageY) / 25,
+          ry = (w / 2 - e.pageX) / 50;
+          theThing.style.transform = 'rotateX(' + rx + 'deg) rotateY(' + -ry + 'deg)'
+          theThing.children[0].style.transform = 'translate3d(' + ry * 3 + 'px, ' + rx * 3 + 'px, 0)'
+    })
+}
